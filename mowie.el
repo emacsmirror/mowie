1c958c328be60966b3531b71708540d84908e42b
Mekeor Melire <mekeor@posteo.de>
2024-04-06 17:25:47 +0200

    elaborate in commentary

--- mowie.el
+++ mowie.el
@@ -30,48 +30,75 @@
 ;;; Commentary:
 
 ;; `mowie' let's you define smart commands that cycle through the
-;; result of point-moving commands by consecutive repetitions. It also
-;; offers a few point-moving commands.
+;; result of point-moving commands by consecutive repetitions. The
+;; package also offers a few point-moving commands.
 
-;; For example, let's define an smart alternative commands to
-;; `beginning-of-line' and `end-of-line':
+;; While repeating the command that you defined using `mowie', it will
+;; skip those point-moving commands that do not provide a point which
+;; has not been visited in the running repetition so far. In a similar
+;; manner, it currently also avoids the point as it was right before
+;; the repetition started.
+
+;; `mowie' strives to be minimalistic rather than being generalizable.
+;; If you want something different, consider copying and modifying the
+;; code to your liking, or using an alternative package.
+
+;; `mowie' differs from Alex Kost's `mwim' package and from Adam
+;; Porter's `mosey' package by being minimalistic rather than complex;
+;; by being opinionated rather than maximizing flexibility; and by not
+;; providing any (rather opaque) macros.
+
+;;; Examples:
+
+;; Cycle through alternatives of `beginning-of-line' and
+;; `end-of-line'!
 ;;
-;;   (require 'mowie)
-;;
-;;   (defun my-mowie-beginning-of-line ()
-;;     "Cycle through alternatives of `beginning-of-line'."
+;;   (defun my-beginning-of-line ()
 ;;     (interactive "^")
 ;;     (mowie
 ;;       #'beginning-of-line
-;;       #'move-beginning-of-line
 ;;       #'beginning-of-visual-line
 ;;       #'mowie-beginning-of-code
 ;;       #'mowie-beginning-of-comment
 ;;       #'mowie-beginning-of-comment-text))
 ;;
-;;   (defun my-mowie-end-of-line ()
-;;     "Cycle through alternatives of `end-of-line'."
+;;   (defun my-end-of-line ()
 ;;     (interactive "^")
 ;;     (mowie
 ;;       #'end-of-line
-;;       #'move-end-of-line
-;;       #'end-of-visual-line))
+;;       #'end-of-visual-line
+;;       #'mowie-end-of-code))
 ;;
 ;;   (keymap-substitute (current-global-map)
-;;     #'move-beginning-of-line #'my-mowie-beginning-of-line)
+;;     #'move-beginning-of-line #'my-beginning-of-line)
 ;;
 ;;   (keymap-substitute (current-global-map)
-;;     #'move-end-of-line #'my-mowie-end-of-line)
+;;     #'move-end-of-line #'my-end-of-line)
 
-;; `mowie' differs from Alex Kost's `mwim' package and from Adam
-;; Porter's `mosey' package by being minimalistic rather than complex;
-;; by being opinionated rather than maximizing flexibility; and by not
-;; providing any macros.
+;; Cycle through alternatives of `beginning-of-buffer' and
+;; `end-of-buffer', e.g. in `message-mode'!
+;;
+;;   (defun my-beginning-of-message ()
+;;     (interactive "^")
+;;     (mowie
+;;       #'message-goto-body
+;;       #'beginning-of-buffer))
+;;
+;;   (defun my-end-of-message ()
+;;     (interactive "^")
+;;     (mowie
+;;       #'message-goto-eoh
+;;       #'end-of-buffer))
+;;
+;;   (keymap-set message-mode-map "M-<" #'my-beginning-of-message)
+;;   (keymap-set message-mode-map "M->" #'my-end-of-message)
 
 ;;; Roadmap:
 
 ;; Introduce a user option determining if last-position should be
 ;; avoided while repeating.
+
+;; Write tests.
 
 ;;; Code:
 
