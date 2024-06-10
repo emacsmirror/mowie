43d1c57a8b576678060ef2a38e06fc1cfe6c0e33
Mekeor Melire <mekeor@posteo.de>
2024-06-10 12:33:21 +0200

    refill commentary

--- mowie.el
+++ mowie.el
@@ -35,9 +35,9 @@
 
 ;; While repeating the command that you defined using `mowie', it will
 ;; skip those point-moving commands that do not provide a point which
-;; has not been visited in the running repetition so far.  In a similar
-;; manner, it currently also avoids the point as it was right before
-;; the repetition started.
+;; has not been visited in the running repetition so far.  In a
+;; similar manner, it currently also avoids the point as it was right
+;; before the repetition started.
 
 ;; `mowie' strives to be minimalistic rather than being generalizable.
 ;; If you want something different, consider copying and modifying the
